de7a7cbc1d600e86dab48db90fcb6c44bf9b11f1
ghaash <daniel.silverman@me.com>
2017-05-25 16:18:19 -0700

    added comments

--- co.js
+++ co.js
@@ -10,12 +10,18 @@
 // cat.name // "Garfield"
 // cat.color // "Oragne"
 
+// defining and creating animal/dog/cat with Animal class
 var animal = new Animal("rawr")
 var dog = new Animal("bark", "Snoopy", "black and white")
 var cat = new Animal("meow","Garfield", "orange" )
 
+//constructor function that
 function Animal(speak, name, color){
   this.speak = speak
   this.name = name
   this.color = color
 }
+
+//dog.speak returns "bark"
+//dog["speak"] returns "bark"
+//dog["speak"] = "arf", dog now returns speak as arf, 
